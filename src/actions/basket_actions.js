102ee1bc1c06769edffdf9468a164a6183f8831b
Igor Morgunov <zxoxz@mail.ru>
2018-05-14 14:05:46 -0600

    w

--- src/actions/basket_actions.js
+++ src/actions/basket_actions.js
@@ -5,9 +5,41 @@ export const SETNEWBASKET = "SETNEWBASKET";
 export const GETDETAILEDFOODINFO = "GETDETAILEDFOODINFO";
 export const GETDETAILEDFOODINFOSUCCESS = "GETDETAILEDFOODINFOSUCCESS";
 export const GETDETAILEDFOODINFOFAILURE = "GETDETAILEDFOODINFOFAILURE";
-
+export const LOGBASKETFOOD = "LOGBASKETFOOD";
+export const LOGBASKETFOODSUCCESS = "LOGBASKETFOODSUCCESS";
+export const LOGBASKETFOODFAILURE = "LOGBASKETFOODFAILURE";
 
 const ROOT_URL = "https://trackapi.nutritionix.com/v2/";
+
+export const logBasketFood = (jwt, basket) => {
+  const headers = {
+    ["x-user-jwt"]: jwt
+  }
+ const obj = {
+   consumed_at: new Date(),
+   foods: basket
+ }
+   const response = axios({
+      method: "POST",
+      url: ROOT_URL + "log",
+      headers,
+      obj
+    });
+
+  return {
+    type: LOGBASKETFOOD,
+    payload: response
+  }
+}
+
+export const logBasketFoodSuccess = () => ({
+  type: LOGBASKETFOODSUCCESS,
+})
+
+export const logBasketFoodFailure = () => ({
+  type: LOGBASKETFOODFAILURE,
+})
+
 
 export const getDetailedFoodInfo = (jwt, foodItem) => {
   const headers = {
